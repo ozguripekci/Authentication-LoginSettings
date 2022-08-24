aad4976e4f932221a8d7acdc8dc1102c94a490a8
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-08-24 22:08:17 +0200

    login required is added

--- main/settings.py
+++ main/settings.py
@@ -126,5 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
