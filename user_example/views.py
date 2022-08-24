a528c84bc2117df13eaee265a2ce11a2c3dffcc4
Ozgur Ipekci <ozgurr.ipekci@gmail.com>
2022-08-24 21:59:35 +0200

    register and change password

---
+++ user_example/views.py
@@ -0,0 +1,32 @@
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+
+
+# Create your views here.
+def home_view(request):
+    return render(request, 'user_example/home.html')
+
+def special(request):
+    return render(request, 'user_example/special.html')
+
+
+def register(request):
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            return redirect('home')
+
+    form = UserCreationForm()        
+    context = {
+        'form' : form
+    }
+    return render(request, 'registration/register.html', context)
